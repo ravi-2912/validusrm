67631794dba0ae0bf6bacb3f53c722244729179c
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-11 22:35:26 +0100

    Added SQL URI.

--- services/server/main.py
+++ services/server/main.py
@@ -6,6 +6,7 @@ import os, subprocess
 os.environ['FLASK_APP'] = r'services\server\api\__init__.py'
 os.environ['FLASK_ENV'] = 'development'
 os.environ['APP_SETTINGS'] = 'api.config.DevelopmentConfig'
+os.environ['DATABASE_URL'] = 'postgres://postgres:postgres@server-db:5432/capital_call_dev'
 
 subprocess.run('python manage.py run')
 
