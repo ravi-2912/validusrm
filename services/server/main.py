4410018cda9baf9dbbf9a222db2755c6ada59240
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-12 00:50:34 +0100

    Added code coverage.

--- services/server/main.py
+++ services/server/main.py
@@ -3,10 +3,11 @@
 import os, subprocess
 
 # Set environment variable
-os.environ['FLASK_APP'] = r'services\server\api\__init__.py'
+#os.environ['FLASK_APP'] = r'services\server\api\__init__.py'
 os.environ['FLASK_ENV'] = 'development'
 os.environ['APP_SETTINGS'] = 'api.config.DevelopmentConfig'
 os.environ['DATABASE_URL'] = 'postgres://postgres:postgres@server-db:5432/capital_call_dev'
 
-subprocess.run('python manage.py test')
+subprocess.run('python manage.py cov')
+print('\n\n')
 subprocess.run('python manage.py run')
--- services/server/manage.py
+++ services/server/manage.py
@@ -1,10 +1,36 @@
-import os, sys, unittest
+import os, sys, unittest, coverage
 from flask.cli import FlaskGroup
 from api import create_app, db
 from api.auth.models import User
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
+
+COV = coverage.coverage(
+    branch=True,
+    include='api/*',
+    omit=[
+        'api/tests/*',
+        'api/config.py',
+    ]
+)
+COV.start()
+
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('api/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    sys.exit(result)
 
 
 @cli.command('recreate_db')
