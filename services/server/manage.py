50c671a86c2751e9eb5462ece82aa4771dbb5351
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-12 00:20:49 +0100

    Added CLI command for running test.

--- services/server/manage.py
+++ services/server/manage.py
@@ -1,10 +1,18 @@
+import os, sys, unittest
 from flask.cli import FlaskGroup
-
 from api import app
 
 
 cli = FlaskGroup(app)
 
+@cli.command()
+def test():
+    """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('api/tests', pattern='test_*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
 
 if __name__ == '__main__':
     cli()
