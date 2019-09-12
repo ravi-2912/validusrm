858ca4a6f0e2a01f32ffb728f8292518ca3457ba
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-12 19:44:42 +0100

    Updated to run locally.

--- services/server/main.py
+++ services/server/main.py
@@ -3,14 +3,14 @@
 import os
 import subprocess
 
+
 # Set environment variable
-# os.environ['FLASK_APP'] = r'services\server\api\__init__.py'
 os.environ['FLASK_ENV'] = 'development'
 os.environ['APP_SETTINGS'] = 'api.config.DevelopmentConfig'
-os.environ['DATABASE_URL'] = '''postgres://postgres:postgres
-                                @server-db:5432/capital_call_dev
-                             '''
+os.environ['DATABASE_URL'] = 'sqlite:///dev.db'
 
-subprocess.run('python manage.py cov')
-print('\n\n')
-subprocess.run('python manage.py run')
+
+if __name__ == '__main__':
+    subprocess.run('python manage.py recreate_db')
+    subprocess.run('python manage.py cov')
+    subprocess.run('python manage.py run')
