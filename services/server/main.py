de5d5621e68696593ba33baf13368b5a9a385c3e
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-12 22:18:38 +0100

    Updated main with CLI.

--- services/server/main.py
+++ services/server/main.py
@@ -1,16 +1,37 @@
 # Local testing file
 
 import os
+import click
 import subprocess
 
 
 # Set environment variable
 os.environ['FLASK_ENV'] = 'development'
-os.environ['DATABASE_URL'] = 'sqlite:///dev.db'
-os.environ['DATABASE_TEST_URL'] = 'sqlite:///test.db'
+os.environ['DATABASE_URL'] = 'sqlite:///local_dev.db'
+os.environ['DATABASE_TEST_URL'] = 'sqlite:///local_test.db'
 os.environ['APP_SETTINGS'] = 'api.config.DevelopmentConfig'
 
 
-if __name__ == '__main__':
-    subprocess.run('python manage.py recreate_db')
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+def cov():
     subprocess.run('python manage.py cov')
+
+
+@click.command()
+def run():
+    subprocess.run('python manage.py recreate_db')
+    subprocess.run('python manage.py seed_db')
+    subprocess.run('python manage.py run')
+
+
+cli.add_command(cov)
+cli.add_command(run)
+
+
+if __name__ == '__main__':
+    cli()
