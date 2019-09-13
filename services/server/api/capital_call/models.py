dc6891fa1d916ff0c1ba4b99eae79dee6f4d891d
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-13 16:37:12 +0100

    Added CapitalCall model.

--- services/server/api/capital_call/models.py
+++ services/server/api/capital_call/models.py
@@ -59,3 +59,33 @@ class Committment(db.Model):
             'amount': self.amount,
             'date': self.date
         }
+
+
+class CapitalCall(db.Model):
+    __tablename__ = 'capitalcall'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(
+        db.DateTime,
+        default=datetime.datetime.now(),
+        nullable=False
+    )
+    investment_name = db.Column(
+        db.String(255),
+        nullable=False,
+        unique=True
+    )
+    capital_requirement = db.Column(db.Float, nullable=False)
+
+    def __init__(self, investment_name, capital_requirement, date=None):
+        self.investment_name = investment_name
+        self.capital_requirement = capital_requirement
+        if date is not None:
+            self.date = date
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'investment_name': self.investment_name,
+            'capital_requirement': self.capital_requirement,
+            'date': self.date
+        }
