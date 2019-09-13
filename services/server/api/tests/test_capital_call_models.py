2c0cfc84ac4c18ba4388428915757b7d22d36678
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-13 12:10:30 +0100

    Added test for fund model.

---
+++ services/server/api/tests/test_capital_call_models.py
@@ -0,0 +1,38 @@
+import unittest
+from sqlalchemy.exc import IntegrityError
+
+from api import db
+from api.capital_call.models import Fund
+from api.tests.base import BaseTestCase
+
+
+def add_fund(fundname):
+    
+    return fund
+
+
+class TestFundModel(BaseTestCase):
+    def test_add_fund(self):
+        """Test to commit a new fund to database"""
+        fund = Fund(fundname='test_fund')
+        db.session.add(fund)
+        db.session.commit()
+        self.assertTrue(fund.id)
+        self.assertEqual(fund.fundname, 'test_fund')
+
+    def test_to_json(self):
+        """Test to to check is JSON is reciwved as dict"""
+        fund = Fund(fundname='test_fund')
+        db.session.add(fund)
+        db.session.commit()
+        self.assertTrue(isinstance(fund.to_json(), dict))
+
+    def test_add_fund_duplicate_name(self):
+        """Test to raise error on adding a duplicate fund name"""
+        fund = Fund(fundname='test_fund')
+        db.session.add(fund)
+        db.session.commit()
+        duplicate_fund = Fund(fundname='test_fund')
+        db.session.add(duplicate_fund)
+        db.session.commit()
+        self.assertRaises(IntegrityError, db.session.commit)
