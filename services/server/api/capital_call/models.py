822fe5316d7975ad2e051c5edf0c967927881440
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-13 16:11:36 +0100

    Updated Fund model and added Committment model.

--- services/server/api/capital_call/models.py
+++ services/server/api/capital_call/models.py
@@ -4,12 +4,17 @@ from api import db
 
 
 class Fund(db.Model):
-
-    __tablename__ = 'funds'
+    __tablename__ = 'fund'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     fundname = db.Column(db.String(128), nullable=False, unique=True)
-    created_date = db.Column(db.DateTime, default=func.now(), nullable=False)
+    date = db.Column(db.DateTime, default=func.now(), nullable=False)
+
+    committments = db.relationship(
+        'Committment',
+        backref='fund',
+        lazy='dynamic'
+    )
 
     def __init__(self, fundname):
         self.fundname = fundname
@@ -18,4 +23,31 @@ class Fund(db.Model):
         return {
             'id': self.id,
             'fundname': self.fundname,
+        }
+
+
+class Committment(db.Model):
+    __tablename__ = 'committment'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    fund_id = db.Column(
+        db.Integer,
+        db.ForeignKey('fund.id'),
+        nullable=False
+    )
+    amount = db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime, default=func.now(), nullable=False)
+
+    def __init__(self, fund_id, amount, date=None):
+        self.amount = amount
+        self.fund_id = fund_id
+        if date is not None:
+            self.date = date
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'fund_id': self.fund_id,
+            'amount': self.amount,
+            'date': self.date
         }
