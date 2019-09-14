504abadcad9a7a08a9a5889a38d813d68c8f3620
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-14 15:08:23 +0100

    Added flask CORS.

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,10 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import "./index.css";
+import UsersList from "./components/UsersList";
+import * as serviceWorker from "./serviceWorker";
+//import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  state = {
+    users: []
+  };
+
+  getUsers = () => {
+    let REACT_APP_USERS_SERVICE_URL = "http://localhost:5000";
+    axios
+      .get(`${REACT_APP_USERS_SERVICE_URL}/users`)
+      .then(res => {
+        let users = res.data.data.users;
+        this.setState({ users });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-one-third">
+              <br />
+              <h1 className="title is-1">All Users</h1>
+              <hr />
+              <br />
+              <UsersList users={this.state.users} />
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
