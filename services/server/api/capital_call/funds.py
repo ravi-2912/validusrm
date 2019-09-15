c66ddf15c42fb0d3a1a0181c1b58c591db4f59fa
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-15 16:14:51 +0100

    Refactored funds API methods.

--- services/server/api/capital_call/funds.py
+++ services/server/api/capital_call/funds.py
@@ -1,138 +1,155 @@
 from flask import request
-from flask_restful import Resource
 from sqlalchemy import exc
+from flask_restful import Resource
 
-from api.capital_call.models import Fund
-from api.capital_call import api
 from api import db
+from api.capital_call import api
+from api.capital_call.models import Fund
+import api.capital_call.utils as UTILS
+
+
+TYPE = 'Fund'
 
 
 class FundsPing(Resource):
+    __name__ = 'FundsPing'
+
     def get(self):
-        return {
-            'status': 'success',
-            'message': 'fund!'
-        }
+        return UTILS.api_response(
+            msg=UTILS.PING_SUCCESS,
+            code=200,
+            data=self.__name__
+        )
 
 
 class FundsList(Resource):
+    __name__ = 'FundsList'
+
     def get(self):
         """Get all funds"""
-        response_object = {
-            'status': 'success',
-            'data': {
-                'funds': [fund.to_json() for fund in Fund.query.all()]
-            }
-        }
-        return response_object, 200
+        funds = Fund.query.all()
+        return UTILS.api_response(
+            msg=UTILS.YES_FUNDSLIST if funds else UTILS.NO_FUNDSLIST,
+            code=200,
+            data={'funds': [fund.to_json() for fund in funds]}
+        )
 
     def post(self):
         post_data = request.get_json()
-        response_object = {
-            'status': 'fail',
-            'message': 'Invalid payload.'
-        }
         if not post_data:
-            return response_object, 400
-
+            return UTILS.api_response(
+                msg=UTILS.INVALID_PAYLD,
+                code=400,
+                data=self.__name__
+            )
         fundname = post_data.get('fundname')
         try:
             fund = Fund.query.filter_by(fundname=fundname).first()
             if not fund:
-                db.session.add(Fund(fundname))
-                db.session.commit()
-                response_object = {
-                    'status': 'success',
-                    'message': f'{fundname} was added!'
-                }
-                return response_object, 201
+                fund = UTILS.add_fund(fundname)
+                return UTILS.api_response(
+                    msg=UTILS.ADDED(TYPE, fundname),
+                    code=201,
+                    data=fund.to_json()
+                )
             else:
-                response_object['message'] = 'Sorry. ' \
-                    'That fund already exists.'
-                return response_object, 400
-        except exc.IntegrityError:
+                return UTILS.api_response(
+                    msg=UTILS.EXISTS(TYPE, fundname),
+                    code=400,
+                    data=fund.to_json()
+                )
+        except exc.IntegrityError as e:
             db.session.rollback()
-            return response_object, 400
+            return UTILS.api_response(
+                msg=f'{UTILS.INTEGRITY_ERR} {self.__name__}',
+                code=400,
+                data=f'{str(e)}'
+            )
 
 
 class Funds(Resource):
+    __name__ = 'Funds'
+
     def get(self, fund_id):
         """Get single fund details"""
-        response_object = {
-            'status': 'fail',
-            'message': 'Fund does not exist'
-        }
         try:
             fund = Fund.query.filter_by(id=int(fund_id)).first()
             if not fund:
-                return response_object, 404
+                return UTILS.api_response(
+                    msg=UTILS.NOT_EXISTS(TYPE, fund_id),
+                    code=404,
+                )
             else:
-                response_object = {
-                    'status': 'success',
-                    'data': fund.to_json()
-                }
-                return response_object, 200
-        except ValueError:
-            return response_object, 404
+                return UTILS.api_response(
+                    msg=UTILS.EXISTS(TYPE, fund.id),
+                    code=200,
+                    data=fund.to_json()
+                )
+        except ValueError as e:
+            return UTILS.api_response(
+                msg=f'{UTILS.VALUE_ERR} {self.__name__}',
+                code=404,
+                data=f'{str(e)}'
+            )
 
     def put(self, fund_id):
         put_data = request.get_json()
+        fundname = put_data.get('fundname')
+        if not put_data:
+            return UTILS.api_response(
+                msg=UTILS.INVALID_PAYLD,
+                code=405
+            )
         try:
             fund = Fund.query.get(fund_id)
-            response_object = {
-                'status': 'fail',
-                'message': 'Invalid payload.',
-                'data': fund.to_json() if fund else []
-            }
-            if not put_data:
-                return response_object, 405
-
-            fundname = put_data.get('fundname')
-            existingFunds = Fund.query.filter_by(fundname=fundname).all()
-            if not existingFunds:
-                if fund:
-                    oldfundname = fund.fundname
-                    fund.fundname = fundname
-                    db.session.commit()
-                    response_object = {
-                        'status': 'success',
-                        'message': f'{oldfundname} was updated to '
-                                   f'{fundname}!',
-                        'data': Fund.query.get(fund_id).to_json()
-                    }
-                    return response_object, 202
-                else:
-                    response_object['message'] = 'Sorry. ' \
-                        'That fund does not exists.'
-                    return response_object, 405
+            existingFunds = Fund.query.filter_by(fundname=fundname).first()
+            if existingFunds:
+                return UTILS.api_response(
+                    msg=UTILS.EXISTS(TYPE, fundname),
+                    code=405,
+                    data=existingFunds.to_json()
+                )
+            if fund:
+                fund = UTILS.update(fund, 'fundname', fundname)
+                return UTILS.api_response(
+                    msg=UTILS.UPDATED(TYPE, fundname),
+                    code=202,
+                    data=Fund.query.get(fund_id).to_json()
+                )
             else:
-                response_object['message'] = 'Sorry. ' \
-                    'That fund already exists.'
-                return response_object, 405
-        except exc.IntegrityError:
+                return UTILS.api_response(
+                    msg=UTILS.NOT_EXISTS(TYPE, fund_id),
+                    code=405,
+                )
+        except exc.IntegrityError as e:
             db.session.rollback()
-            return response_object, 405
+            return UTILS.api_response(
+                msg=f'{UTILS.INTEGRITY_ERR} {self.__name__}',
+                code=405,
+                data=f'{str(e)}'
+            )
 
     def delete(self, fund_id):
         """Delete single fund details"""
-        response_object = {
-            'status': 'fail',
-            'message': 'Fund does not exist'
-        }
         try:
             fund = Fund.query.filter_by(id=int(fund_id)).first()
             if not fund:
-                return response_object, 404
+                return UTILS.api_response(
+                    msg=UTILS.NOT_EXISTS(TYPE, fund_id),
+                    code=404,
+                )
             else:
-                db.session.delete(fund)
-                db.session.commit()
-                response_object = {
-                    'status': 'success',
-                    'message': f'{fund.fundname} successfully deleted.'
-                }
-                return response_object, 200
-        except ValueError:
-            return response_object, 404
+                UTILS.delete(fund)
+                return UTILS.api_response(
+                    msg=UTILS.DELETED(TYPE, fund.id),
+                    code=200
+                )
+        except ValueError as e:
+            return UTILS.api_response(
+                msg=f'{UTILS.VALUE_ERR} {self.__name__}',
+                code=404,
+                data=f'{str(e)}'
+            )
 
 
 api.add_resource(FundsPing, '/funds/ping')
