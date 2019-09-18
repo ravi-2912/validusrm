8e22be65ab7a14068c58944eeddcee8b5f7bf21d
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-18 23:19:31 +0100

    Updated to seed database.

--- services/server/manage.py
+++ services/server/manage.py
@@ -1,13 +1,14 @@
 import sys
 import unittest
 from flask.cli import FlaskGroup
-from datetime.datetime import strptime
+from datetime import datetime
 
 from testcoverage import COV
 from api import create_app, db
 from api.auth.users import User
 from api.capital_call.funds import Fund
 from api.capital_call.committments import Committment
+from api.capital_call.capital_calls import CapitalCall
 import api.capital_call.utils as UTILS
 
 
@@ -49,19 +50,28 @@ def seed_db():
     UTILS.add_fund('fund_2')
     UTILS.add_fund('fund_3')
     UTILS.add_fund('fund_4')
-    UTILS.add_committment(1, 10000000, strptime('31/12/2017', '%d/%m/%Y'))
-    UTILS.add_committment(2, 15000000, strptime('31/03/2018', '%d/%m/%Y'))
-    UTILS.add_committment(3, 10000000, strptime('30/06/2018', '%d/%m/%Y'))
-    UTILS.add_committment(4, 15000000, strptime('30/09/2018', '%d/%m/%Y'))
-    UTILS.add_committment(1, 10000000, strptime('31/12/2018', '%d/%m/%Y'))
+    UTILS.add_committment(1, 10000000, datetime.
+                          strptime('31/12/2017', '%d/%m/%Y'))
+    UTILS.add_committment(2, 15000000, datetime.
+                          strptime('31/03/2018', '%d/%m/%Y'))
+    UTILS.add_committment(3, 10000000, datetime.
+                          strptime('30/06/2018', '%d/%m/%Y'))
+    UTILS.add_committment(4, 15000000, datetime.
+                          strptime('30/09/2018', '%d/%m/%Y'))
+    UTILS.add_committment(1, 10000000, datetime.
+                          strptime('31/12/2018', '%d/%m/%Y'))
+    UTILS.add_capitalcall('investment_1', 9500000, datetime.
+                          strptime('31/01/2018', '%d/%m/%Y'))
+    UTILS.add_capitalcall('investment_2', 10000000, datetime.
+                          strptime('30/04/2018', '%d/%m/%Y'))
 
 
 @cli.command()
 def test():
     """Runs the tests without code coverage"""
-    #tests = unittest.TestLoader().discover('api/tests', pattern='test_*.py')
-    from api.tests.test_funds import TestFundsService
-    tests = unittest.TestLoader().loadTestsFromTestCase(TestFundsService)
+    tests = unittest.TestLoader().discover('api/tests', pattern='test_*.py')
+    #from api.tests.test_funds import TestFundsService
+    #tests = unittest.TestLoader().loadTestsFromTestCase(TestFundsService)
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
