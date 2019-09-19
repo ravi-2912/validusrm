73d985a9769c2d682a1196599b85fc860ddb346f
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-20 00:05:24 +0100

    Updated imports.

--- services/server/api/capital_call/utils.py
+++ services/server/api/capital_call/utils.py
@@ -1,7 +1,7 @@
 from api.capital_call.funds import Fund
 from api.capital_call.committments import Committment
 from api.capital_call.capital_calls import CapitalCall
-from api.capital_call.fundinvestments import Investment
+from api.capital_call.investments import Investment
 from api import db
 
 
