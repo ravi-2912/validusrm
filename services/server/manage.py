88142513dada791c02a9c8c90a5dcb853a20b288
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-23 00:39:39 +0100

    Refactored code.

--- services/server/manage.py
+++ services/server/manage.py
@@ -46,38 +46,45 @@ def seed_db():
     db.session.add(User(username='ravi', email="ravi@gmail.com"))
     db.session.add(User(username='ravisingh', email="ravisingh@hotmail.org"))
     db.session.commit()
-    fund_1 = UTILS.add_fund('fund_1')
-    fund_2 = UTILS.add_fund('fund_2')
-    fund_3 = UTILS.add_fund('fund_3')
-    fund_4 = UTILS.add_fund('fund_4')
-    fund_5 = UTILS.add_fund('fund_5')
-    committment_1 = UTILS.add_committment(1, 10000000, datetime.
-                          strptime('31/12/2017', '%d/%m/%Y'))
-    committment_2 = UTILS.add_committment(2, 15000000, datetime.
-                          strptime('31/03/2018', '%d/%m/%Y'))
-    committment_3 = UTILS.add_committment(3, 10000000, datetime.
-                          strptime('30/06/2018', '%d/%m/%Y'))
-    committment_4 = UTILS.add_committment(4, 15000000, datetime.
-                          strptime('30/09/2018', '%d/%m/%Y'))
-    committment_5 = UTILS.add_committment(1, 10000000, datetime.
-                          strptime('31/12/2018', '%d/%m/%Y'))
-    
-    capitalcall_1 = UTILS.add_capitalcall('investment_1', 9500000, datetime.
-                          strptime('31/01/2018', '%d/%m/%Y'))
-    capitalcall_2 = UTILS.add_capitalcall('investment_2', 10000000, datetime.
-                          strptime('30/04/2018', '%d/%m/%Y'))
-    
-    UTILS.add_fundinvestment(9500000, fund_1, committment_1, capitalcall_1)
-    UTILS.add_fundinvestment(500000, fund_1, committment_1, capitalcall_2)
-    UTILS.add_fundinvestment(9500000, fund_2, committment_2, capitalcall_2)
+    UTILS.add_fund('fund_1')
+    UTILS.add_fund('fund_2')
+    UTILS.add_fund('fund_3')
+    UTILS.add_fund('fund_4')
+    UTILS.add_fund('fund_5')
+    committment_1 = UTILS.add_committment(
+        1, 10000000, datetime.strptime('31/12/2017', '%d/%m/%Y')
+    )
+    committment_2 = UTILS.add_committment(
+        2, 15000000, datetime.strptime('31/03/2018', '%d/%m/%Y')
+    )
+    UTILS.add_committment(
+        3, 10000000, datetime.strptime('30/06/2018', '%d/%m/%Y')
+    )
+    UTILS.add_committment(
+        4, 15000000, datetime.strptime('30/09/2018', '%d/%m/%Y')
+    )
+    UTILS.add_committment(
+        1, 10000000, datetime.strptime('31/12/2018', '%d/%m/%Y')
+    )
+
+    capitalcall_1 = UTILS.add_capitalcall(
+        'investment_1', 9500000, datetime.strptime('31/01/2018', '%d/%m/%Y')
+    )
+    capitalcall_2 = UTILS.add_capitalcall(
+        'investment_2', 10000000, datetime.strptime('30/04/2018', '%d/%m/%Y')
+    )
+
+    UTILS.add_fundinvestment(9500000, committment_1, capitalcall_1)
+    UTILS.add_fundinvestment(500000, committment_1, capitalcall_2)
+    UTILS.add_fundinvestment(9500000, committment_2, capitalcall_2)
 
 
 @cli.command()
 def test():
     """Runs the tests without code coverage"""
     #tests = unittest.TestLoader().discover('api/tests', pattern='test_*.py')
-    from api.tests.test_capitalcalls import TestCapitalCallsService
-    tests = unittest.TestLoader().loadTestsFromTestCase(TestCapitalCallsService)
+    from api.tests.test_investments import TestInvestmentsService
+    tests = unittest.TestLoader().loadTestsFromTestCase(TestInvestmentsService)
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
