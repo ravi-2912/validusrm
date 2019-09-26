81a2b6fe64017546b9b717634bb9651d31765e91
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-26 08:17:35 +0100

    Added currency formatter and calcs for funds.

--- services/client/src/components/helper.js
+++ services/client/src/components/helper.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import * as Numeral from 'numeral';
 import { ProgressBar } from 'react-bootstrap';
 
-export const ProgressBarFormatter = data => {
-  const value = data.value.value;
-  const totalInvested = data.value.totalInvested;
-  const totalCommitted = data.value.totalCommitted;
-  return (
-    <div>
-      <ProgressBar striped now={value} label={`$ ${totalInvested} / $ ${totalCommitted}`} />
-    </div>
-  );
+export const currencyFormat = val => {
+  let cur = Numeral(val).format('0.00a');
+  const curSplitted = cur.split('.');
+  const dec = curSplitted[1].slice(0, 2);
+  if (dec === '00') {
+    cur = curSplitted[0] + curSplitted[1].slice(2);
+  }
+  return cur;
 };
 
 export const defaultColumnProperties = {
@@ -20,4 +20,24 @@ export const DateFormatter = date => {
   const dateString = date.value.toString().split(' ')[0];
 
   return <div>{dateString}</div>;
+};
+
+export const calcs_for_funds_invested_committed = (committments, fund = undefined) => {
+  let totalCommitted = 0;
+  let totalInvested = 0;
+  for (let c of committments) {
+    totalCommitted += c.amount;
+    for (let i of c.investments) {
+      totalInvested += i.investment;
+    }
+  }
+  // added this for progressbar values
+  const invested_committed = {
+    fund_id: fund ? fund.id : 0,
+    fund_name: fund ? fund.name : '',
+    value: totalCommitted === 0 ? 0 : (100 * totalInvested) / totalCommitted,
+    totalCommitted: totalCommitted,
+    totalInvested: totalInvested,
+  };
+  return invested_committed;
 };
