a37a36809695e5fbc719708a58c63123d4bf688d
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-26 08:15:44 +0100

    Added route for capital call investments app.

--- services/client/src/components/Router.js
+++ services/client/src/components/Router.js
@@ -5,6 +5,7 @@ import NotFound from './NotFound';
 import DataGrid from './DataGrid';
 import NavBar from './NavBar';
 import FundsManagement from './FundsManagement';
+import CapitalCallsManagement from './CapitalCallsManagement';
 
 const Router = () => (
   <BrowserRouter>
@@ -12,6 +13,7 @@ const Router = () => (
       <Route exact path="/grid" component={DataGrid} />
       <Route exact path="/nav" component={NavBar} />
       <Route exact path="/:direction(funds|committments)" component={FundsManagement} />
+      <Route exact path="/:direction(dashboard|newcall)" component={CapitalCallsManagement} />
       <Route exact path="/" component={App} />
       <Route component={NotFound} />
     </Switch>
