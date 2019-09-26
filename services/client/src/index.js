bb29f529382a42a5e264627a76b17efbc934c4f4
Ravinder Singh <ravi_29_12@hotmail.com>
2019-09-26 23:26:39 +0100

    Imported index css.

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Router from './components/Router';
 import * as serviceWorker from './serviceWorker';
 
+import './css/index.css';
+
 ReactDOM.render(<Router />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
